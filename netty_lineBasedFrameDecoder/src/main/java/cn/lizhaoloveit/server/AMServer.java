56737a7ab0c99d59f6843b142af32cb404540060
Ammar <lacuself@126.com>
2020-09-26 01:19:26 +0800

    no message

---
+++ netty_lineBasedFrameDecoder/src/main/java/cn/lizhaoloveit/server/AMServer.java
@@ -0,0 +1,46 @@
+package cn.lizhaoloveit.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+/**
+ * DESCRIPTION:
+ * Author: ammar
+ * Date:   2020-09-25
+ * Time:   16:38
+ */
+public class AMServer {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup parentGroup = new NioEventLoopGroup();
+        NioEventLoopGroup childGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(parentGroup, childGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new LineBasedFrameDecoder(5120)); // 最大长度 5k 否则抛异常
+                        pipeline.addLast(new StringDecoder());
+                        pipeline.addLast(new AMServerHandler());
+                    }
+                });
+        try {
+            ChannelFuture future = bootstrap.bind(8888).sync();
+            System.out.println("服务器已启动");
+            future.channel().closeFuture().sync();
+        } finally {
+            parentGroup.shutdownGracefully();
+            childGroup.shutdownGracefully();
+        }
+    }
+}
