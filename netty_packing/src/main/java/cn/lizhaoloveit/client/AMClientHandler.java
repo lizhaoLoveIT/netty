014be017fdfdd013820f2cafdbe42f5222aa182f
Ammar <lacuself@126.com>
2020-09-25 23:30:08 +0800

    no message

--- netty_packing/src/main/java/cn/lizhaoloveit/client/AMClientHandler.java
+++ netty_packing/src/main/java/cn/lizhaoloveit/client/AMClientHandler.java
@@ -13,84 +13,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  */
 public class AMClientHandler extends ChannelInboundHandlerAdapter {
 
-    String message = "Netty is a NIO client server framework " +
-            "which enables quick and easy development of network applications " +
-            "such as protocol servers and clients. It greatly simplifies and " +
-            "streamlines network programming such as TCP and UDP socket server." +
-            "'Quick and easy' doesn't mean that a resulting application will " +
-            "suffer from a maintainability or a performance issue. Netty has " +
-            "this guide and play with Netty.In other words, Netty is an NIO  " +
-            "framework that enables quick and easy development of network  " +
-            "as protocol servers and clients. It greatly simplifies and network " +
-            "programming such as TCP and UDP socket server development.'Quick " +
-            "not mean that a resulting application will suffer from a maintain" +
-            "performance issue. Netty has been designed carefully with the expe " +
-            "from the implementation of a lot of protocols such as FTP, SMTP, " +
-            " binary and text-based legacy protocols. As a result, Netty has " +
-            "a way to achieve of development, performance, stability, without " +
-            "which enables quick and easy development of network applications " +
-            "such as protocol servers and clients. It greatly simplifies and " +
-            "streamlines network programming such as TCP and UDP socket server." +
-            "'Quick and easy' doesn't mean that a resulting application will " +
-            "suffer from a maintainability or a performance issue. Netty has " +
-            "this guide and play with Netty.In other words, Netty is an NIO  " +
-            "framework that enables quick and easy development of network  " +
-            "as protocol servers and clients. It greatly simplifies and network " +
-            "programming such as TCP and UDP socket server development.'Quick " +
-            "not mean that a resulting application will suffer from a maintain" +
-            "performance issue. Netty has been designed carefully with the expe " +
-            "from the implementation of a lot of protocols such as FTP, SMTP, " +
-            " binary and text-based legacy protocols. As a result, Netty has " +
-            "a way to achieve of development, performance, stability, without " +
-            "which enables quick and easy development of network applications " +
-            "such as protocol servers and clients. It greatly simplifies and " +
-            "streamlines network programming such as TCP and UDP socket server." +
-            "'Quick and easy' doesn't mean that a resulting application will " +
-            "suffer from a maintainability or a performance issue. Netty has " +
-            "this guide and play with Netty.In other words, Netty is an NIO  " +
-            "framework that enables quick and easy development of network  " +
-            "as protocol servers and clients. It greatly simplifies and network " +
-            "programming such as TCP and UDP socket server development.'Quick " +
-            "not mean that a resulting application will suffer from a maintain" +
-            "performance issue. Netty has been designed carefully with the expe " +
-            "from the implementation of a lot of protocols such as FTP, SMTP, " +
-            " binary and text-based legacy protocols. As a result, Netty has " +
-            "a way to achieve of development, performance, stability, without " +
-            "which enables quick and easy development of network applications " +
-            "such as protocol servers and clients. It greatly simplifies and " +
-            "framework that enables quick and easy development of network  " +
-            "as protocol servers and clients. It greatly simplifies and network " +
-            "programming such as TCP and UDP socket server development.'Quick " +
-            "not mean that a resulting application will suffer from a maintain" +
-            "performance issue. Netty has been designed carefully with the expe " +
-            "from the implementation of a lot of protocols such as FTP, SMTP, " +
-            " binary and text-based legacy protocols. As a result, Netty has " +
-            "a way to achieve of development, performance, stability, without " +
-            "which enables quick and easy development of network applications " +
-            "such as protocol servers and clients. It greatly simplifies and " +
-            "framework that enables quick and easy development of network  " +
-            "as protocol servers and clients. It greatly simplifies and network " +
-            "programming such as TCP and UDP socket server development.'Quick " +
-            "not mean that a resulting application will suffer from a maintain" +
-            "performance issue. Netty has been designed carefully with the expe " +
-            "from the implementation of a lot of protocols such as FTP, SMTP, " +
-            " binary and text-based legacy protocols. As a result, Netty has " +
-            "a way to achieve of development, performance, stability, without " +
-            "which enables quick and easy development of network applications " +
-            "such as protocol servers and clients. It greatly simplifies and " +
-            "streamlines network programming such as TCP and UDP socket server." +
-            "'Quick and easy' doesn't mean that a resulting application will " +
-            "suffer from a maintainability or a performance issue. Netty has " +
-            "this guide and play with Netty.In other words, Netty is an NIO  " +
-            "framework that enables quick and easy development of network  " +
-            "as protocol servers and clients. It greatly simplifies and network " +
-            "programming such as TCP and UDP socket server development.'Quick " +
-            "not mean that a resulting application will suffer from a maintain" +
-            "performance issue. Netty has been designed carefully with the expe " +
-            "from the implementation of a lot of protocols such as FTP, SMTP, " +
-            " binary and text-based legacy protocols. As a result, Netty has " +
-            "a way to achieve of development, performance, stability, without " +
-            "a compromise.=====================================================";
+    String message = "hello word";
 
     /**
      * @param ctx
@@ -98,19 +21,9 @@ public class AMClientHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-//        ctx.channel().writeAndFlush(message);
-//        ctx.channel().writeAndFlush(message);
-        byte[] bytes = message.getBytes();
-        ByteBuf buffer = null;
-        for (int i = 0; i < 2; i++) {
-            // 申请缓存空间
-            buffer = Unpooled.buffer(bytes.length);
-            // 将数据写入到缓存
-            buffer.writeBytes(bytes);
-            // 将缓存中的数据写入到 channel
-            ctx.writeAndFlush(buffer);
+        for (int i = 0; i < 200; i++) {
+            ctx.writeAndFlush(message);
         }
-        // 效果与注释一样
     }
 
     @Override
