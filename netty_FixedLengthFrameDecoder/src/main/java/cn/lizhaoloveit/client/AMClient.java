ca3a51d91d8c10e2b0db4682553cb8fb4ccc35ef
Ammar <lacuself@126.com>
2020-09-26 02:30:42 +0800

    no message

---
+++ netty_FixedLengthFrameDecoder/src/main/java/cn/lizhaoloveit/client/AMClient.java
@@ -0,0 +1,42 @@
+package cn.lizhaoloveit.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+/**
+ * DESCRIPTION:
+ * Author: ammar
+ * Date:   2020-09-25
+ * Time:   17:02
+ */
+public class AMClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast(new StringEncoder())
+                                    .addLast(new StringDecoder())
+                                    .addLast(new AMClientHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect("localhost", 8888).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ netty_FixedLengthFrameDecoder/src/main/java/cn/lizhaoloveit/server/AMServer.java
@@ -0,0 +1,47 @@
+package cn.lizhaoloveit.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+/**
+ * DESCRIPTION:
+ * Author: ammar
+ * Date:   2020-09-25
+ * Time:   16:38
+ */
+public class AMServer {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup parentGroup = new NioEventLoopGroup();
+        NioEventLoopGroup childGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(parentGroup, childGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new FixedLengthFrameDecoder(11));
+                        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                        pipeline.addLast(new AMServerHandler());
+                    }
+                });
+        try {
+            ChannelFuture future = bootstrap.bind(8888).sync();
+            System.out.println("服务器已启动");
+            future.channel().closeFuture().sync();
+        } finally {
+            parentGroup.shutdownGracefully();
+            childGroup.shutdownGracefully();
+        }
+    }
+}
